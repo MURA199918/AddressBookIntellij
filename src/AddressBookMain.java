48a4d1be3c4904896d89a418029f6292b4a13ab3
MURA199918 <muralisaranr18@gmail.com>
2020-11-27 14:38:38 +0530

    added UC1

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,3 +1,13 @@
+class contact{
+    String firstname;
+    String lastname;
+    String address;
+    String city;
+    String state;
+    int zip;
+    int phoneno;
+    String email;
+}
 public class AddressBookMain {
     public static void main(String args[]){
         System.out.println(".......Welcome to Address Book.......");
